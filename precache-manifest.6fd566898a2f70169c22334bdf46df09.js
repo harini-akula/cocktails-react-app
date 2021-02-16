fc413d625d5eaeb74066831c7dfcec5d3549898c
unknown <harini.akula01@gmail.com>
2021-02-16 05:33:29 -0500

    Updates

---
+++ precache-manifest.6fd566898a2f70169c22334bdf46df09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b2fabc3c60e94c66038dcde64d6044",
+    "url": "/cocktails-react-app/index.html"
+  },
+  {
+    "revision": "1b846610b1fd9c6503be",
+    "url": "/cocktails-react-app/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "4e64cb41c467386471a9",
+    "url": "/cocktails-react-app/static/js/2.4322b41b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktails-react-app/static/js/2.4322b41b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b846610b1fd9c6503be",
+    "url": "/cocktails-react-app/static/js/main.c69e14a7.chunk.js"
+  },
+  {
+    "revision": "41169e94d06e12d0fc9b",
+    "url": "/cocktails-react-app/static/js/runtime-main.7fe9c043.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktails-react-app/static/media/logo.9a3d2645.svg"
+  }
+]);
